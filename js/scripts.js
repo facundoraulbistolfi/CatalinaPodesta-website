ffc3cd55e5b69ceb8918c273e43befe66a6d3aec
facundoraulbistolfi <facundobistolfi@hotmail.com>
2020-05-29 22:40:57 -0300

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -44,24 +44,10 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
 
-  $('form').on('submit', function (e) {
+  $('#formContacto').on('submit', function (e) {
 
+    console.log("Envio contacto");
     e.preventDefault();
 
     $.ajax({
